936d43b886c2d005ace3afcc0f068d6db576e42e
Catriel Pérez <perezcatriel@gmail.com>
2022-09-02 16:55:29 -0300

    Agregando funcionalidades al carrito de compras

--- src/main.mjs
+++ src/main.mjs
@@ -5,21 +5,24 @@ function like(id) {
 	const precio = document.getElementById("precio-" + id).textContent;
 
 	if (id !== idRepetido) {
-		showArt(articulo, precio);
+		showArt(articulo, precio, id);
 		idRepetido = id;
+	} else {
+		borrarArt(id);
 	}
 }
 
-function showArt(articulo, precio) {
+function showArt(articulo, precio, id) {
 	const articulos = document.querySelector(".articulos");
+	console.log(articulo, precio, id);
 
 	const art = document.createElement("div");
 	art.className = "articuloCompra";
-	art.id = articulo;
+	art.id = id;
 
 	const eliminar = document.createElement("img");
 	eliminar.src = "./assets/borrar.svg";
-	eliminar.addEventListener("click", () => borrarArt(articulo));
+	eliminar.addEventListener("click", () => borrarArt(id));
 
 	const nombre = document.createElement("p");
 	nombre.textContent = articulo;
@@ -28,15 +31,22 @@ function showArt(articulo, precio) {
 
 	const restar = document.createElement("img");
 	restar.src = "./assets/restar.svg";
-	restar.addEventListener("click", () => restarArt(articulo));
+	restar.addEventListener("click", () => restarArt(articulo, id));
 	const cantidad = document.createElement("strong");
 	cantidad.textContent = "1";
-	cantidad.id = "cantidad";
+	cantidad.id = "cantidad-" + id;
 	const sumar = document.createElement("img");
 	sumar.src = "./assets/sumar.svg";
-	sumar.addEventListener("click", () => sumarArt(articulo));
+	sumar.addEventListener("click", () => sumarArt(articulo, id));
 	const check = document.createElement("img");
 	check.src = "./assets/comprar.svg";
+
+	const cantidadTotal = document.getElementById('cantidad-total')
+	cantidadTotal.innerText = Number(cantidad)
+	const pagoTotal = document.getElementById('pago-total')
+	pagoTotal.innerText = sumaPrecio(precio, cantidad)
+
+	console.log(cantidadTotal)
 
 	art.append(eliminar);
 	art.append(nombre);
@@ -48,39 +58,59 @@ function showArt(articulo, precio) {
 
 	articulos.append(art);
 
-	// console.log(articulo, precio);
+	cantidadProductos(id, precio)
+	totalPagar()
 }
 
-function borrarArt(articulo) {
-	const art = document.getElementById(articulo);
+let precioTotal = 0
+function sumaPrecio (precio, cantidad) {
+console.log(cantidad)
+
+	return (precioTotal += Number(precio)).toFixed(2)
+}
+
+function cantidadProductos(id, precio) {
+	console.log(id, precio)
+}
+
+function totalPagar() {}
+
+
+function borrarArt(id) {
+	const art = document.getElementById(id);
+	idRepetido = "";
 
 	art.remove();
 }
 
-function restarArt(articulo) {
-	let cant = document.getElementById("cantidad").textContent;
-
-	if (cant > 1) cant--;
-
-	console.log("restar", articulo, cant);
-}
-
 let articuloTotal = 1;
 
-function sumarArt(cantidad) {
-	const $cantidad = document.getElementById("cantidad").textContent;
+function restarArt(cantidad, id) {
+	const $cantidad = document.getElementById("cantidad-" + id).textContent;
+
+	function resta() {
+		articuloTotal--;
+		$cantidad.textContent = articuloTotal;
+		recarga(String(cantidad), articuloTotal, id);
+	}
+
+	if (articuloTotal > 1) resta();
+}
+
+function sumarArt(cantidad, id) {
+	const $cantidad = document.getElementById("cantidad-" + id).textContent;
 
 	function suma() {
 		articuloTotal++;
 		$cantidad.textContent = articuloTotal;
-		recarga(String(cantidad), articuloTotal);
+		recarga(String(cantidad), articuloTotal, id);
 	}
 
 	suma();
 }
 
-function recarga($cantidad, articuloTotal) {
-	const cantidad = document.getElementById("cantidad");
+function recarga($cantidad, articuloTotal, id) {
+	const cantidad = document.getElementById("cantidad-" + id);
 
 	cantidad.textContent = articuloTotal;
 }
